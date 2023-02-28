ec82af8478c7d8d9a91f0b97af07675bd62ab663
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-02-28 23:40:42 +0530

    sum of diagonal

---
+++ pr25.c
@@ -0,0 +1,39 @@
+//sum of diagonal with one testcase
+#include<stdio.h>
+void main(){
+        int rows;
+        int cols;
+        int sum=0;
+        printf("enter rows:\n");
+        scanf("%d",&rows);
+        printf("enter cols:\n");
+        scanf("%d",&cols);                                
+	int arr[rows][cols];   
+        if(rows==cols){	
+       	printf("enter array elements:\n");                
+	for(int i=0;i<rows;i++){
+           for(int j=0;j<cols;j++){
+               scanf("%d",&arr[i][j]);
+           }
+         }
+           printf("\n");
+           printf("2D Array:");
+        for(int i=0;i<rows;i++){
+           for(int j=0;j<cols;j++){
+               printf("|%d|",arr[i][j]);                     }                                               }
+        printf("\n");
+        printf("Diagonal addition\n");
+        
+        for(int i=0;i<rows;i++){
+           for(int j=0;j<cols;j++){
+               if(arr[i]==arr[j]){
+                 sum=sum+arr[i][j];
+               }
+           }
+         }
+        printf("\n");                              
+        printf("sum of diagonal elements:%d\n",sum);       }
+else{
+    printf("For addition of diagonal ele.rows &cols must be equal ,enter equal no.of.rows &cols!\n");
+  }
+}
